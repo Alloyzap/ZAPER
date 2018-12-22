dd1d80233709c83e8fca550e8efbf5d4c7127402
Alloyzap <45991495+Alloyzap@users.noreply.github.com>
2018-12-22 22:33:57 +0530

    Add files via upload

---
+++ Zaper.py
@@ -0,0 +1,35 @@
+# i just took a and b as to test this thing out...
+a = ('ckl')
+b = ('hcnmoaxqe')
+ba = ('enfbffbvgfbvncxazx')
+ab = ('ijvoltnrrytuvgtbinwtrevcoejv')
+q = ckl = 'a'                        # i was too lazy to write the whole thing
+w = hcnmoaxqe = 'b'
+r = enfbffbvgfbvncxazx = 'ba'      # we can not use Q.W.R.T as <<-- 
+t = ijvoltnrrytuvgtbinwtrevcoejv = 'ab'
+
+X = input(' Text To Be encrypted or decrypted') # it is not possiable to write every word
+
+if X == a:                                 # there i need your help
+    print (a)
+elif X == b:
+    print(b)
+elif X ==ab:
+    print(ab)
+elif X == ba:
+    print(ba)
+else:
+    print ('Its:')
+
+if X == ckl :
+        print (q)
+elif X == hcnmoaxqe :
+        print(w)
+elif X == enfbffbvgfbvncxazx :
+        print(r)
+elif X == ijvoltnrrytuvgtbinwtrevcoejv :
+        print(t)
+else:
+        print ('Its:')
+
+# thank you , Harsh
